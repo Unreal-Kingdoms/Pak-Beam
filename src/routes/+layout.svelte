b9cb0661495c233c0ccbfaa3992ab327fd514048
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-04-07 02:38:37 -0400

    Framework laid

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.pcss";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, ImagePlaceholder, Skeleton, TextPlaceholder } from 'flowbite-svelte';
+</script>
+  
+  <div class="relative px-8">
+    <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
+      <NavBrand href="/">
+        <img src="/assets/branding/main-logo.png" class="me-3 h-10 sm:h-9" alt="PakBeam Logo" />
+      </NavBrand>
+      <NavHamburger />
+      <NavUl>
+        <NavLi href="/experiences" active={true}>Experiences</NavLi>
+        <NavLi href="/pak-manager">Pak Manager</NavLi>
+        <NavLi href="https://github.com/Unreal-Kingdoms/Pak-Beam/wiki">Docs</NavLi>
+        <NavLi href="https://github.com/Unreal-Kingdoms/Pak-Beam">GitHub</NavLi>
+      </NavUl>
+    </Navbar>
+    <div class="content">
+        <slot></slot>
+    </div>
+  </div>
